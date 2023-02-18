a84c36d9ecf85990181b2c744b7cb6e5c30920fb
NimmiShashikala60 <nimmishashikala385@gmail.com>
2023-02-18 16:21:17 +0530

    new update

--- Backend/src/main/java/lk/ijse/spring/controller/AdminInformationController.java
+++ Backend/src/main/java/lk/ijse/spring/controller/AdminInformationController.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping()
 public class AdminInformationController {
+
+    @GetMapping
+    public void Customer(){
+        System.out.println("get invoked");
+    }
 }
